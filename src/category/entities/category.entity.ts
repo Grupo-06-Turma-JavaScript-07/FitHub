dd094385ecfa7e31343d72065f7cae97815334b2
Matheus Schneider <matheusschneider503@gmail.com>
2025-06-27 11:44:47 -0300

    subindo a Entities e a Service

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,1 +1,16 @@
-export class Category {}
+import { IsNotEmpty } from 'class-validator';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from '../../user/entities/user.entity';
+
+@Entity({ name: 'tb_categories' })
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  category: string;
+
+  @OneToMany(() => User, (user) => user.category)
+  user: User[];
+}
--- src/category/service/category.service.ts
+++ src/category/service/category.service.ts
@@ -1,1 +1,63 @@
-export class Category {}
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, ILike, Repository } from 'typeorm';
+import { Category } from '../entities/category.entity';
+
+@Injectable()
+export class CategoryService {
+  constructor(
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>,
+  ) {}
+
+  async findAll(): Promise<Category[]> {
+    return await this.categoryRepository.find({
+      relations: {
+        user: true,
+      },
+    });
+  }
+
+  async findById(id: number): Promise<Category> {
+    const category = await this.categoryRepository.findOne({
+      where: {
+        id,
+      },
+      relations: {
+        user: true,
+      },
+    });
+
+    if (!category)
+      throw new HttpException('Category not found', HttpStatus.NOT_FOUND);
+
+    return category;
+  }
+
+  async findAllByCategories(categories: string): Promise<Category[]> {
+    return await this.categoryRepository.find({
+      where: {
+        categories: ILike(`%${categories}%`),
+      },
+      relations: {
+        user: true,
+      },
+    });
+  }
+
+  async create(Category: Category): Promise<Category> {
+    return await this.categoryRepository.save(Category);
+  }
+
+  async update(category: Category): Promise<Category> {
+    await this.findById(category.id);
+
+    return await this.categoryRepository.save(category);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    await this.findById(id);
+
+    return await this.categoryRepository.delete(id);
+  }
+}
